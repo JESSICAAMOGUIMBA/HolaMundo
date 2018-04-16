2ea2cfc794f2ab8d45b0344895a253b5e3db7b94
JESSICAAMOGUIMBA <38404267+JESSICAAMOGUIMBA@users.noreply.github.com>
2018-04-16 08:53:29 -0500

    Add files via upload

---
+++ multiplicacion/src/multiplicacion/Multiplicacion.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multiplicacion;
+
+/**
+ *
+ * @author ESFOT
+ */
+public class Multiplicacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("\t\tTabla de Multiplicar");
+        System.out.println("2 x 1 = 2");
+        System.out.println("2 x 2 = 4");
+        System.out.println("2 x 3 = 6");
+        System.out.println("2 x 4 = 8");
+        System.out.println("2 x 5 = 10");
+        System.out.println("2 x 6 = 12");
+        System.out.println("2 x 7 = 14");
+        System.out.println("2 x 8 = 16");
+        System.out.println("2 x 9 = 18");
+        System.out.println("2 x 10 = 20");
+        System.out.println("2 x 11 = 22");
+        System.out.println("2 x 12 = 24");
+    }
+    
+}
